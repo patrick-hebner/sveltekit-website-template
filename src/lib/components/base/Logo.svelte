33973e6d150dfbdbd0c86f8feede59e5075baacf
hetec <hebner.tec@gmail.com>
2023-09-13 17:14:02 +0200

    feat: add logo component

---
+++ src/lib/components/base/Logo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/cn';
+
+	export let href = '/';
+	export let classes = '';
+</script>
+
+<a {href} class={cn('text-primary-500 w-fit h-fit', classes)}>
+	<svg width="46" height="34" viewBox="0 0 46 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M46 0H30L0 34H16L46 0Z" fill="currentColor" />
+		<path
+			d="M38 34C42.4183 34 46 30.5206 46 26.2286C46 21.9365 42.4183 18.4571 38 18.4571C33.5817 18.4571 30 21.9365 30 26.2286C30 30.5206 33.5817 34 38 34Z"
+			fill="currentColor"
+		/>
+	</svg>
+</a>
--- src/lib/components/footers/SimpleCenteredFooter.svelte
+++ src/lib/components/footers/SimpleCenteredFooter.svelte
@@ -1,14 +1,15 @@
 <script>
-	import Container from '../base/Container.svelte';
-	import Link from '../base/Link.svelte';
-	import Section from '../base/Section.svelte';
-	import Spacer from '../base/Spacer.svelte';
+	import Container from '$lib/components/base/Container.svelte';
+	import Logo from '$lib/components/base/Logo.svelte';
+	import Link from '$lib/components/base/Link.svelte';
+	import Section from '$lib/components/base/Section.svelte';
+	import Spacer from '$lib/components/base/Spacer.svelte';
 </script>
 
 <Section tag="footer">
 	<Spacer size="small" />
-	<Container classes="flex flex-col justify-center text-center gap-4">
-		<div class="text-xl text-primary-500">Logo</div>
+	<Container classes="flex flex-col justify-center items-center gap-4">
+		<Logo />
 		<nav class="flex justify-center gap-10">
 			<Link href="/services">Services</Link>
 			<Link href="/about">About</Link>
